6cef6c94c7b874e16fa00fb7e129b65579a52667
gloriaJun <pureainu@gmail.com>
2019-03-25 23:10:46 +0900

    add example about html2canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import html2canvas from 'html2canvas';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.imgRefs = React.createRef();
+    this.resultRefs = React.createRef();
+    this.state = {
+      canvas: null,
+    };
+  };
+
+  onClickSnapshot = async () => {
+    const canvas = await html2canvas(this.imgRefs.current, {
+      // allowTaint: true,
+      useCORS: true,
+    });
+    this.setState({
+      canvas: canvas.toDataURL(),
+    })
+    this.resultRefs.current.appendChild(canvas);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.onClickSnapshot}>capture</button>
+        <a download="myImage.jpg" href={this.state.canvas}>Download to myImage.jpg</a>
+        <div ref={this.imgRefs}>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4nsgRjLZntKqK6j0kKjHsYJtQQGsLo27TeDJhy3p85qp9m9WB" />
+          {/*<img crossOrigin="Anonymous" src="https://c8.staticflickr.com/8/7492/15644563231_4d682b3c88_n.jpg" alt="The Scream" />*/}
+          {/*<img src="https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png" alt="html2canvas demo"/>*/}
+        </div>
+        <img src={this.state.canvas} alt="test" />
+        <div ref={this.resultRefs} />
       </div>
     );
   }
