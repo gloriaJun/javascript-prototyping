79ad5e604f05d2755658fc789853c671bfa513d4
gloriaJun <pureainu@gmail.com>
2019-03-25 23:32:42 +0900

    update example about html2canvas cors

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ class App extends Component {
         <a download="myImage.jpg" href={this.state.canvas}>Download to myImage.jpg</a>
         <div ref={this.imgRefs}>
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4nsgRjLZntKqK6j0kKjHsYJtQQGsLo27TeDJhy3p85qp9m9WB" />
-          {/*<img crossOrigin="Anonymous" src="https://c8.staticflickr.com/8/7492/15644563231_4d682b3c88_n.jpg" alt="The Scream" />*/}
-          {/*<img src="https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png" alt="html2canvas demo"/>*/}
+          <img src="http://localhost:8080/public/profile.jpeg" />
         </div>
         <img src={this.state.canvas} alt="test" />
         <div ref={this.resultRefs} />
